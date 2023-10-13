30557b8eece70b3e60458cddba2b9bf5a88a5706
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-10-13 18:46:27 -0500

    Images

--- lib/ui/pages/PanelPrincipal/widgets/room_card.dart
+++ lib/ui/pages/PanelPrincipal/widgets/room_card.dart
@@ -6,45 +6,34 @@ class RoomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-  onTap: () {
-    // Navegar a la otra ruta cuando se presiona la tarjeta
-    Navigator.pushNamed(context, '/room/register');
-  },
-  onHover: (value) => const Text("Habitaciones"),
-  child: Card(
-    
-    elevation: 5,
-    shape: RoundedRectangleBorder(
-      side: BorderSide(
-        color: Theme.of(context).colorScheme.outline,
-      ),
-      borderRadius: const BorderRadius.all(Radius.circular(20)),
-    ),
-    margin: EdgeInsets.zero,
- 
-    child: SizedBox(
-      
-      height: MediaQuery.of(context).size.height * 0.400,
-      
-      child: Container(
-
-        alignment: Alignment.center,
-        
-        
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset('../../../../../assets/images/room.png',height: 400,),
-          
+      onTap: () {
+        // Navegar a la otra ruta cuando se presiona la tarjeta
+        Navigator.pushNamed(context, '/room/register');
+      },
+      onHover: (value) => const Text("Habitaciones"),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
           ),
-        
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        margin: EdgeInsets.zero,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.400,
+          child: Container(
+            alignment: Alignment.center,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                'assets/images/Inicio.png',
+                height: 400,
+              ),
+            ),
+          ),
+        ),
       ),
-      
-      
-    ),
-    
-    
-    
-  ),
-);
+    );
   }
 }
